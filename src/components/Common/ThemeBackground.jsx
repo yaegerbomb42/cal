851070c059f35366b1dec4c3f4c9f3353ce411c7
yaegerbomb42 <yaeger.james42@gmail.com>
2026-01-13 01:12:48 -0600

    fix: correct import path in ThemeBackground.jsx

--- src/components/Common/ThemeBackground.jsx
+++ src/components/Common/ThemeBackground.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react';
-import { useTheme } from '../contexts/ThemeContext';
+import { useTheme } from '../../contexts/ThemeContext';
 
 const ThemeBackground = () => {
     const { theme } = useTheme();
